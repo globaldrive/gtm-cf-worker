b9c8f043647dd14481b096bff0ec413a5adc027a
Sergey Indenbom <dzorogh@gmail.com>
2023-08-23 16:17:34 +0300

    feat: universal id

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 // Change to your own GTM container
-const GTM_URL = `https://www.googletagmanager.com/gtm.js?id=GTM-MPZZWMSR`; 
-const CACHE_EXPIRY = 30; // cache timeout in seconds
+const GTM_URL = `https://www.googletagmanager.com/gtm.js`;
+const CACHE_EXPIRY = 5; // cache timeout in seconds
 
 // Listen to any incoming requests and apply our function
 addEventListener('fetch', function(event) {
@@ -20,14 +20,21 @@ async function handleRequest(event) {
   // https://developers.cloudflare.com/workers/examples/cache-api
   const cacheURL = new URL(request.url);
 
+	const gtmId = cacheURL.searchParams.get('id');
+
   const cacheKey = new Request(cacheURL.toString(), request);
   const cache = caches.default
 
   let response = await cache.match(cacheKey);
 
+	console.log(response);
+
   if (!response) {
+		const gtmUrl = new URL(GTM_URL)
+		gtmUrl.searchParams.set('id', gtmId)
+
     // If not in cache, get it from origin
-    response = await fetch(GTM_URL);
+    response = await fetch(gtmUrl);
 
     // Must use Response constructor to inherit all of response's fields
     response = new Response(response.body, response)
